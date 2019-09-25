fbb18e9d4c26fa48aafe4b60c64dd32cb043d567
Brian Becker (alienufo) <alien13ufo@gmail.com>
2019-09-24 21:11:57 -0400

    Sound Effect when picking up item
    - Added a sound effect when picking up an item (configurable)
    - Renamed PlayerControler class to PlayerController (don't misspell class names!)
    - Reworked collision detection for power ups so that it is now on the PowerUp class instead of the PlayerController class.  This makes more sense structurally.

--- ah/Assets/PowerUp.cs
+++ ah/Assets/PowerUp.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,17 +7,23 @@ public class PowerUp : MonoBehaviour
 {
 	public enum PowerUpTypes {smallHeal, fullHeal, dmgIncrease, haste, invincibility, hpIncrease, barrier};
 	public int myPower = 0;
+	public PlayerController player;
+	private AudioSource soundEffect;
+	private AudioClip aClip;
 	//private Sprite sprite;
 
 
     // Start is called before the first frame update
     void Start()
     {
+		soundEffect = GetComponent<AudioSource>();
+		aClip = soundEffect.clip;
 		if(myPower >= 0 && myPower < System.Enum.GetNames(typeof(PowerUp.PowerUpTypes)).Length) // makes sure the number is within the scope of PowerUpTypes
 		{
 			
 		}
     }
+
 
 	public int myPowerType()
 	{
@@ -28,6 +35,30 @@ public class PowerUp : MonoBehaviour
 		return System.Enum.GetName(typeof(PowerUpTypes), myPower);
 	}
 
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		if(col.gameObject.name == "player")
+		{
+			
+			Debug.Log("We have picked up a " + Enum.GetName(typeof(PowerUpTypes), myPower) + " powerup!");
+
+			if(myPower == 0) // small heal
+			{
+				Debug.Log("HP: " + player.getHpCurrent() + "/" + player.getHpMax());
+				player.changeHpCurrent(3);  // just test numbers
+				Debug.Log("HP: " + player.getHpCurrent() + "/" + player.getHpMax());
+			}
+			else if(myPower == 1) // full heal
+			{
+				Debug.Log("HP: " + player.getHpCurrent() + "/" + player.getHpMax());
+				player.changeHpCurrent(999);
+				Debug.Log("HP: " + player.getHpCurrent() + "/" + player.getHpMax());
+			}
+			soundEffect.PlayOneShot(aClip);
+			GetComponent<SpriteRenderer>().enabled = false;
+			Destroy(gameObject, aClip.length); // destroy when done
+		}
+	}
 
     // Update is called once per frame
     void Update()

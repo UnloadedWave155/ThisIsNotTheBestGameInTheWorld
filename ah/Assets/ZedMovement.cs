0e3507eb17e12ceaed158e073f6641e159b8cab3
UnloadedWave155 <48725609+UnloadedWave155@users.noreply.github.com>
2019-10-17 12:17:07 -0400

    had to revert back to an older version
    Had to merge the "Added Artwork" commit beacause A): The character's animations were glitched (back to moonwalking.) and B): I tried to get a ghoul spawner working. It worked so well it managed to literaty break the ghoul's movement and collision.
    ~Joey Nash

--- ah/Assets/ZedMovement.cs
+++ ah/Assets/ZedMovement.cs
@@ -1,27 +1,20 @@
-﻿using System.Collections;
+﻿﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ZedMovement : MonoBehaviour
 {
 	public float moveSpeed;
-	public float zedSpeed;
 	public int moveX;
-	private bool alive;
-	//public GameObject Spawn;
-	//public GameObject Foe;
 	
 	private Rigidbody2D myRigidbody;
-	//implement the animation later.
+	//later implement the animation later.
 	private Animator anim; 
 	
     // Start is called before the first frame update
     void Start()
     {
-		
-		alive = true;
         myRigidbody = GetComponent<Rigidbody2D>();
-		//gameObject.transform.position = Spawn.transform.position;
 		moveX= -1;
     }
 
@@ -29,23 +22,11 @@ public class ZedMovement : MonoBehaviour
     void Update()
     {
 		//case for zombie moving left, works like a charm
-        if(moveX<=0 ){
-			myRigidbody.velocity = new Vector2( zedSpeed * -moveSpeed, myRigidbody.velocity.y);
-		}
-		if(gameObject.transform.position.y<50){
-			Destroy(gameObject);
-			alive=false;
-		}
-		/*if(alive==false){
-			respawn();
-			
+        if(moveX<=0){
+			myRigidbody.velocity = new Vector2( 1f * -moveSpeed, myRigidbody.velocity.y);
 		}
     }
-	void respawn(){
-		Instantiate(Foe,Spawn.transform.position,Quaternion.identity);
-		alive = true;*/
-       
-	}
+
 	
 		
 	void OnCollisionEnter2D (Collision2D col)
@@ -54,7 +35,7 @@ public class ZedMovement : MonoBehaviour
 			
 			
 			Destroy(gameObject);
-			alive=false;
+			
 		
 		}
 	}

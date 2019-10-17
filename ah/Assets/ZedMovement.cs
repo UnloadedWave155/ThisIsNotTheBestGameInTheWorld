200ff62bc5d39a28cce7d94041203fac34cbc986
UnloadedWave155 <48725609+UnloadedWave155@users.noreply.github.com>
2019-10-17 12:03:13 -0400

    don't use this version
    it's basiciclly broke

--- ah/Assets/ZedMovement.cs
+++ ah/Assets/ZedMovement.cs
@@ -7,15 +7,21 @@ public class ZedMovement : MonoBehaviour
 	public float moveSpeed;
 	public float zedSpeed;
 	public int moveX;
+	private bool alive;
+	//public GameObject Spawn;
+	//public GameObject Foe;
 	
 	private Rigidbody2D myRigidbody;
-	//later implement the animation later.
+	//implement the animation later.
 	private Animator anim; 
 	
     // Start is called before the first frame update
     void Start()
     {
+		
+		alive = true;
         myRigidbody = GetComponent<Rigidbody2D>();
+		//gameObject.transform.position = Spawn.transform.position;
 		moveX= -1;
     }
 
@@ -23,10 +29,23 @@ public class ZedMovement : MonoBehaviour
     void Update()
     {
 		//case for zombie moving left, works like a charm
-        if(moveX<=0){
+        if(moveX<=0 ){
 			myRigidbody.velocity = new Vector2( zedSpeed * -moveSpeed, myRigidbody.velocity.y);
 		}
+		if(gameObject.transform.position.y<50){
+			Destroy(gameObject);
+			alive=false;
+		}
+		/*if(alive==false){
+			respawn();
+			
+		}
     }
+	void respawn(){
+		Instantiate(Foe,Spawn.transform.position,Quaternion.identity);
+		alive = true;*/
+       
+	}
 	
 		
 	void OnCollisionEnter2D (Collision2D col)
@@ -35,6 +54,7 @@ public class ZedMovement : MonoBehaviour
 			
 			
 			Destroy(gameObject);
+			alive=false;
 		
 		}
 	}
